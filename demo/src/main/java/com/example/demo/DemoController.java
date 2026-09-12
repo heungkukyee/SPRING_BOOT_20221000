1d1639713cd3e18714cbe4827e9fce5ded18dceb
heungkukyee <silkroad0924@gmail.com>
2026-09-12 15:29:57 +0900

    2주차 연습문제 URL 매핑과 컨트롤러 추가 완료

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -8,7 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DemoController {
   @GetMapping("/hello") // 전송 방식 GET
   public String hello(Model model) {
-    model.addAttribute("data", " 반갑습니다."); // model 설정
+    model.addAttribute("data", "반갑습니다."); // model 설정
     return "hello"; // hello.html 연결
+  }
+
+  @GetMapping("/hello2")
+  public String hello2(Model model) {
+    model.addAttribute("name", "유장건님");
+    model.addAttribute("hi", "반갑습니다.");
+    model.addAttribute("today", "오늘");
+    model.addAttribute("weather", "날씨는");
+    model.addAttribute("good", "매우 좋습니다.");
+    return "hello2";
   }
 }
